c49aef4aa83dc14ee8725777d3e88775a58b5b0c
ʍᴙ.ώoŁƒ <gaiththewolf@users.noreply.github.com>
2018-04-23 12:13:06 +0200

    Update Code

--- GitCls.php
+++ GitCls.php
@@ -18,8 +18,12 @@
 
         public function Count_Commits()
         {   
+            exec('git status -z -u', $status);
             $b = exec('git symbolic-ref --short HEAD');
+            exec('git rev-parse '.$b, $rev_parse);
+            exec('git rev-parse --symbolic-full-name '.$b.'@{u}', $rev_parse2);
             exec('git rev-list --left-right '.$b.'...refs/remotes/origin/'.$b,$output);
+            exec('git for-each-ref --format %(refname) %(objectname) --sort -committerdate', $each_ref);
             $in=array();; $out=array();;
             foreach ($output as $line) {
                 $line = (strpos($line, '>') !== false) ? $in[] = $line : $out[] = $line ;
